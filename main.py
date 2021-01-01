734432b8b18ce19d6fc7cc538fad22c21159af29
arshpatel47 <75131820+arshpatel47@users.noreply.github.com>
2021-01-01 21:47:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+print('Welcome to A softwaring !!')
+print('What is your name :')
+name=input()
+print('Hi',name)
+
+print('what is your Hobby:')
+Hobby=input()
+print(Hobby,'WOW!')
+
+print('Thank You',name)
+print('Visit our Chatbot')
+
+
+
+
+
+
+
+
+
+
